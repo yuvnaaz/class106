45c601f6b54c21e09e523248d53625e831167737
yuvnaaz <69728010+yuvnaaz@users.noreply.github.com>
2021-07-24 10:53:16 -0700

    Add files via upload

---
+++ class106.py
@@ -0,0 +1,21 @@
+import plotly.express as px
+import csv 
+import numpy as np
+
+def getDataSource(data_path):
+    ice_cream_sales = []
+    cold_drink_sales = []
+    with open(data_path, encoding = "utf-8") as csv_file:
+         csv_reader = csv.DictReader(csv_file)
+         for row in csv_reader:
+             ice_cream_sales.append(float(row['Temperature']))
+             cold_drink_sales.append(float(row['Ice-cream Sales( ₹ )']))
+    return{'x': ice_cream_sales, 'y': cold_drink_sales}
+def findCorrelation(datasource):
+    correlation = np.corrcoef(datasource['x'],datasource['y'])
+    print('correlation is ', correlation[0,1])
+def setup():
+    data_path = 'data.csv'
+    datasource = getDataSource(data_path)
+    findCorrelation(datasource)
+setup()
